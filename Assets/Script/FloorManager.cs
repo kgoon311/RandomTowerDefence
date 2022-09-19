6466fcd01eacf25f7f1bd16b27af0a0835850f4c
kgoon <sdh210203@sdh.hs.kr>
2022-09-19 20:52:18 +0900

    Turret Base 40%

--- Assets/Script/FloorManager.cs
+++ Assets/Script/FloorManager.cs
@@ -15,21 +15,26 @@ public class FloorManager : MonoBehaviour
     [SerializeField] private Vector2 FirstDigFloorPos;
     [SerializeField] private Vector2 EndDigFloorPos;
     [SerializeField] private bool DigEnd;
-
     private Vector2 RecentDigFloor;
     private List<Vector3Int> DiggingFloor = new List<Vector3Int>();
-    private LayerMask Grassmask;
-    private LayerMask Digmask;
-
     [Header("MousePointer")]
     [SerializeField] GameObject M_Object;
     [SerializeField] Sprite[] M_Images = new Sprite[2];
     private SpriteRenderer M_Sprite;
     private Vector3 mousePosition;
+    [Header("Turret")]
+    [SerializeField] GameObject umm;
+    private bool SelectTurret;
+    
+    private LayerMask Grassmask;
+    private LayerMask turretmask;
+    private LayerMask Digmask;
+
 
     void Start()
     {
-        Grassmask = LayerMask.GetMask("Floor") | LayerMask.GetMask("Turret");
+        Grassmask = LayerMask.GetMask("Floor");
+        turretmask = LayerMask.GetMask("Turret");
         Digmask = LayerMask.GetMask("DigFloor");
         M_Sprite = M_Object.GetComponent<SpriteRenderer>();
         ResetFloor();
@@ -40,35 +45,29 @@ public class FloorManager : MonoBehaviour
     {
         mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePosition = new Vector2(Mathf.Floor(mousePosition.x) + 0.5f, Mathf.Floor(mousePosition.y) + 0.5f);
-        if (Input.GetMouseButtonDown(0) && DigEnd == false)
-        {
-            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            RaycastHit2D hit = Physics2D.Raycast(pos, transform.forward, 100000f, Grassmask);
-            if (hit)
-            {
-                DigFloor(mousePosition);
-            }
-        }
         if (Input.GetKeyDown(KeyCode.R) && DigEnd == false)
         {
             ResetFloor();
         }
+        if (Input.GetMouseButtonDown(0) )
+        {
+            RaycastHit2D Glasshit = Physics2D.Raycast(mousePosition, transform.forward, 100000f, Grassmask);
+            RaycastHit2D TurretHit = Physics2D.Raycast(mousePosition, transform.forward, 100000f, turretmask);
+            if (Glasshit && DigEnd == false)
+            {
+                DigFloor(mousePosition);
+            }
+            else if(SelectTurret == true)
+            {
+                BuildTurret();
+            }
+            else if(TurretHit)
+            { 
+                TurretInformation(TurretHit.transform.gameObject);
+            }
+        }
+       
         MousePointerSpawn();
-    }
-    void MousePointerSpawn()
-    {
-        M_Object.transform.position = mousePosition;
-        RaycastHit2D hit = Physics2D.Raycast(mousePosition, transform.forward, 10000f, Grassmask);
-        Collider2D hit1 = Physics2D.OverlapBox(mousePosition, new Vector2(0.9f, 2.9f), 0, Digmask);//세로 오버랩박스
-        Collider2D hit2 = Physics2D.OverlapBox(mousePosition, new Vector2(2.9f, 0.9f), 0, Digmask);//가로 오버렙박스
-        if (hit&&hit1 ==null&&hit2==null && Mathf.Sqrt(Mathf.Pow(RecentDigFloor.x - mousePosition.x, 2) + Mathf.Pow(RecentDigFloor.y - mousePosition.y, 2)) == 1)
-        {
-            M_Sprite.sprite = M_Images[0];
-        }
-        else
-        {
-            M_Sprite.sprite = M_Images[1];
-        }
     }
     void ResetFloor()
     {
@@ -87,17 +86,16 @@ public class FloorManager : MonoBehaviour
     }
     void DigFloor(Vector2 TargetFloor)
     {
-        /* List<GameObject> Fourdirection = new List<GameObject>();// 십자가 모양으로 받아오기
-          bool Dig = false;*/
         Collider2D hit = Physics2D.OverlapBox(TargetFloor, new Vector2(0.9f, 2.9f), 0, Digmask);//세로 오버랩박스
         Collider2D hit2 = Physics2D.OverlapBox(TargetFloor, new Vector2(2.9f, 0.9f), 0, Digmask);//가로 오버렙박스
-        if (hit == null && hit2 == null && Mathf.Sqrt(Mathf.Pow(RecentDigFloor.x - TargetFloor.x, 2) + Mathf.Pow(RecentDigFloor.y - TargetFloor.y, 2)) == 1)//바로 전에 자신의 블록이 있는지 체크
+        if (hit == null && hit2 == null && Vector2.Distance(TargetFloor, RecentDigFloor) == 1)//바로 전에 자신의 블록이 있는지 체크
         {
-            Vector3Int TilePos = GrassTileMap.WorldToCell(TargetFloor);
+            Vector3Int TilePos = GrassTileMap.WorldToCell(TargetFloor);//마우스 클릭 위치를 타입맵 기준으로 바꿔주는 함수
             DiggingFloor.Add(TilePos);
             GrassTileMap.SetTile(TilePos, null);
-            if (EndDigFloorPos != TargetFloor)//마지막 블록이 아니라면 페이크 흙 설치
+            if (EndDigFloorPos != TargetFloor)
             {
+                //오버랩박스에 안걸리기 위해 흙블록을 페이크타일맵에 적용
                 FakeDirt.SetTile(TilePos, Dirt);
                 TilePos = GrassTileMap.WorldToCell(RecentDigFloor);
                 DigTileMap.SetTile(TilePos, Dirt);
@@ -113,36 +111,34 @@ public class FloorManager : MonoBehaviour
             }
 
         }
-        /*foreach (Collider2D a in Physics2D.OverlapBoxAll(TargetFloor, new Vector2(0.9f, 2.9f), 0, Digmask))//세로 오버랩박스
-        {
-            Fourdirection.Add(a.transform.gameObject);
-        }
-        foreach (Collider2D a in Physics2D.OverlapBoxAll(TargetFloor, new Vector2(0.9f, 2.9f), 90, Digmask))//가로 오버렙박스
-        {
-            Fourdirection.Add(a.transform.gameObject);
-        }
-        int RecentIdxfloor = Fourdirection.IndexOf(RecentDigFloor);//주변 땅중 바로 전에 판 땅이 있는지 체크
-        if (RecentIdxfloor > -1)//있다면 실행
-        {
-            Fourdirection.RemoveAt(RecentIdxfloor);//리스트에서 전 땅 삭제
-            Fourdirection.RemoveAll(a => a == TargetFloor);//리스트에서 자신 땅 삭제
+    }
+    void BuildTurret()
+    {
 
-            foreach (GameObject i in Fourdirection)//주변 모든 땅이 파졌는지 안파졌는지 체크
-            {
-                if (i.GetComponent<turret>().Diging)
-                    Dig = true;
-            }
-            if (Dig == false)//안파져있다면 클릭한 땅을 파기
-            {
-                TargetFloor.GetComponent<turret>().Diging = true;
-                TargetFloor.GetComponent<SpriteRenderer>().color = Color.clear;
-                DiggingFloor.Add(TargetFloor);
-                RecentDigFloor = TargetFloor;
-                if (TargetFloor == EndDigFloor)
-                    DigEnd = true;
-            }
+    }
+    void TurretInformation(GameObject Turret)
+    {
+
+    }
+    void MousePointerSpawn()
+    {
+        M_Object.transform.position = mousePosition;
+        RaycastHit2D Glasshit = Physics2D.Raycast(mousePosition, transform.forward, 10000f, Grassmask);
+        RaycastHit2D TurretHit = Physics2D.Raycast(mousePosition, transform.forward, 10000f, turretmask);
+        Collider2D V_DigHit = Physics2D.OverlapBox(mousePosition, new Vector2(0.9f, 2.9f), 0, Digmask);//세로 오버랩박스
+        Collider2D H_DigHit = Physics2D.OverlapBox(mousePosition, new Vector2(2.9f, 0.9f), 0, Digmask);//가로 오버렙박스
+        if (Glasshit&& V_DigHit == null&& H_DigHit == null&& Vector2.Distance(mousePosition,RecentDigFloor) == 1)
+        {
+            M_Sprite.sprite = M_Images[0];
         }
-        */
+        else if(DigEnd == true && TurretHit)
+        {
+            M_Sprite.sprite = M_Images[0];
+        }
+        else
+        {
+            M_Sprite.sprite = M_Images[1];
+        }
     }
 
 }
--- Assets/Script/turret.cs
+++ Assets/Script/turret.cs
@@ -2,12 +2,64 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Type
+{
+    Missile,
+    Electricity,
+    bullet,
+    Laser,
+    Slow,
+    ATKSpeedUp,
+    ATKDmgUp,
+}
 public class turret : MonoBehaviour
 {
-    public bool Diging;
-    void Update()
+    public Type TurretType;
+    public int Dmg;
+    public int ATKSpeed;
+    public int Buf_ATKSpeed;
+    public int Rank;
+    private Vector3 BeforePos;
+    private Vector3 MousePos;
+    private bool Move;
+    private void Update()
     {
-        
+        if(Move)
+        {
+            MousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = new Vector3(Mathf.Floor(MousePos.x) + 0.5f, Mathf.Floor(MousePos.y) + 0.5f, -1);
+        }
     }
-    
+    private void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Move = true;
+            BeforePos = transform.position;
+        }
+        else if(Input.GetMouseButtonUp(0))
+        {
+            Move = false;
+            transform.position = BeforePos;
+        }
+    }
 }
+abstract class ATK : turret
+{
+    protected void Start()
+    {
+        StartCoroutine("Attack");
+    }
+    private IEnumerator Attack()
+    {
+        yield return new WaitForSeconds(ATKSpeed - Buf_ATKSpeed);
+        AttackPattern();
+        yield return StartCoroutine("Attack");
+    }
+    abstract protected void AttackPattern();
+}
+abstract class Buffer : turret
+{
+
+}
+
